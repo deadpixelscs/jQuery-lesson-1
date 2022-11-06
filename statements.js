b0674b3d5c385c2c115934ef3de91592d77da537
Pete Machin <designer@deadpixelsdesign.com>
2022-11-06 21:55:46 +0000

    Update statements.js

--- statements.js
+++ statements.js
@@ -36,3 +36,30 @@ $("a:last");
 
 // Select all header elements (h1, h2, h3, h4, h5, h6)
 $(":header");
+
+// Get all img elements on the page
+$("img");
+
+// Get all elements with a class of `card_image`
+$(".card_image");
+
+// Get the footer by its ID
+$('#my_footer');
+
+// All paragrapgh elements in the `footer`
+$("footer p");
+
+// Get all header elements (h1-h6)
+$(":header");
+
+// Get first element on the page
+$(":first");
+
+// Get the last `div` on the page
+$("div:last")
+
+// Get the last `img` on the page
+$("img:last");
+
+// Get all elements that have an attribute of `href`
+$("[href]")
